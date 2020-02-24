37f3c83e54df504ca021c1bb31612925dda3660e
Hohloho <krtcch121@gmail.com>
2020-02-24 15:09:10 +0700

    test

---
+++ week5/ex2/src/main.cpp
@@ -0,0 +1,42 @@
+#include <Arduino.h>
+#include <ThreeWire.h> 
+#include <RtcDS1302.h>
+
+ThreeWire myWire(4,5,2); // IO, SCLK, CE 
+RtcDS1302<ThreeWire> Rtc(myWire);
+int ledPin = 13;
+byte prevSec = 0;
+
+void printDateTime(const RtcDateTime& dt){ 
+  Serial.print("Total minutes : ");
+  Serial.print(dt.Minute(), DEC); 
+  Serial.print(':'); 
+  Serial.print(dt.Second(), DEC); 
+  Serial.println();
+}
+
+void setup() {
+  Serial.begin(9600);
+  Rtc.Begin();
+  RtcDateTime setting_time(0);
+  Rtc.SetDateTime(setting_time); // set time to be like setting_time
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  RtcDateTime now = Rtc.GetDateTime();
+  printDateTime(now);
+  int second = now.Second();
+  int minute = now.Minute();
+  if second = 0; 
+  if(second%2==0){
+    digitalWrite(ledPin,HIGH);
+  }else
+  {
+    digitalWrite(ledPin,LOW);
+  }
+  if(minute=2){
+    Serial.println("End")
+  }
+  delay(1000);
+}
